abb28e8f7ea22691e64026c83243269630e6dce8
KARANAM KARTHIK <karthik.karanam@clr.com>
2026-04-28 14:23:09 +0530

    testing prod

--- app/app.py
+++ app/app.py
@@ -4,4 +4,4 @@ import streamlit as st
 env = os.getenv("ENV", "dev")
 
 st.title("Databricks App 🚀")
-st.write(f"Running in **{env}** environment triggerd by nagendra")
+st.write(f"Running in prod environment triggerd by nagendra")
